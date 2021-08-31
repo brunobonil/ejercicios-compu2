827fd9363ecce9a3b83ecb704a3b72e0412f1fb4
brunobonil <brunobonil@gmail.com>
2021-08-31 00:27:44 -0300

    Final

---
+++ remote_shell_multicli/client.py
@@ -0,0 +1,28 @@
+import socket
+import argparse
+import os
+import datetime
+
+SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 8080
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((SERVER, PORT))
+
+def enviar():
+    while True:
+        mensaje = input("Ingrese el comando: ")
+        cliente.send(mensaje.encode('utf-8'))
+        output = cliente.recv(2048).decode('utf-8')
+        print(output)
+        if args.log:
+            fd = os.open(args.log, os.O_RDWR | os.O_CREAT)
+            os.write(fd, output.encode('utf-8'))
+            fecha = str(datetime.datetime.today())
+            os.write(fd, fecha.encode('utf-8'))
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--log', type=str, help='Crea un archivo donde se almacenará el log de la sesión')
+args = parser.parse_args()
+enviar()
---
+++ remote_shell_multicli/server.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+import subprocess
+
+PORT = 8080
+SERVER = socket.gethostbyname(socket.gethostname())
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((SERVER, PORT))
+
+def manejo_cliente(conn, address):
+    print(f'Nueva conexión: {address}')
+    while True:
+        command = conn.recv(2048).decode('utf-8')
+        out = subprocess.run(command.split(), capture_output=True)
+        if out.returncode != 0:
+            conn.send(bytes((f'[ERROR]\n{out.stderr.decode("utf-8")}'), 'utf-8'))
+        else:
+            conn.send(bytes((f'[OK]\n{out.stdout.decode("utf-8")}'), 'utf-8'))
+
+
+def start():
+    server.listen()
+    while True:
+        conn, address = server.accept()
+        thread = threading.Thread(target=manejo_cliente, args=(conn, address))
+        thread.start()
+
+print("*SERVIDOR INICIALIZADO*")
+start()
